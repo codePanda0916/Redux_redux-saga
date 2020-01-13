a8cce444bbb705b717e2deb0668113f044d67db4
xiongyujie <xyj0916@126.com>
2020-01-13 23:35:32 +0800

    add redux-saga

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 cnpm install
 npm start
 ```
+### redux-saga的安装
+```javascript
+cnpm install --save redux-saga
+```
+### 引入并创建redux-saga
+```javascript
+import createSagaMiddleware from 'redux-saga';  // 引入saga
+const sagaMiddleware = createSagaMiddleware();  // 创建saga中间件
+```
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga'; // 引入saga中间件
 import reducer from './reducer';
+const sagaMiddleware = createSagaMiddleware(); // 创建saga中间件
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 export default store;
